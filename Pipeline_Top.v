9e99b3e86e5d950ef1543e32d437978f186df00f
Mahadev133 <mr.mahadevwadgire@gmail.com>
2026-01-17 14:28:35 +0530

    Update Pipeline_Top.v

--- Pipeline_Top.v
+++ Pipeline_Top.v
@@ -1,32 +1,3 @@
-// Copyright 2023 MERL-DSU
-
-//    Licensed under the Apache License, Version 2.0 (the "License");
-//    you may not use this file except in compliance with the License.
-//    You may obtain a copy of the License at
-
-//        http://www.apache.org/licenses/LICENSE-2.0
-
-//    Unless required by applicable law or agreed to in writing, software
-//    distributed under the License is distributed on an "AS IS" BASIS,
-//    WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-//    See the License for the specific language governing permissions and
-//    limitations under the License.
-
-`include "Fetch_Cycle.v"
-`include "Decode_Cyle.v"
-`include "Execute_Cycle.v"
-`include "Memory_Cycle.v"
-`include "Writeback_Cycle.v"    
-`include "PC.v"
-`include "PC_Adder.v"
-`include "Mux.v"
-`include "Instruction_Memory.v"
-`include "Control_Unit_Top.v"
-`include "Register_File.v"
-`include "Sign_Extend.v"
-`include "ALU.v"
-`include "Data_Memory.v"
-`include "Hazard_unit.v"
 
 
 module Pipeline_top(clk, rst);
